5f6efebd2a4e6ae2c1c0bad959716ea7eac65aa7
Carlos A. Gomes <carlos.algms@gmail.com>
2014-11-18 15:48:36 -0200

    changed the template to use bootstrat. configured navbar and behavior on small devices to show the toggle button on the left.

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,9 +1,9 @@
-﻿
+
 module.exports = function ( grunt, options ) {
 
     return {
         options: {
-            spawn: false,
+            spawn: false
         },
         configFiles: {
             files: ['Gruntfile.js', 'grunt/*.js'],
@@ -19,21 +19,21 @@ module.exports = function ( grunt, options ) {
             files: '<%= concat.jsLibs.src %>',
             tasks: ['concat:jsLibs', 'uglify:jsLibs'],
             options: {
-                debounceDelay: 500,
+                debounceDelay: 500
             }
         },
         jsSite: {
             files: '<%= concat.jsSite.src %>',
             tasks: ['concat:jsSite', 'uglify:jsSite'],
             options: {
-                debounceDelay: 500,
+                debounceDelay: 500
             }
         },
         sassSite: {
-            files: ['assets/scss/site.scss'],
+            files: ['assets/scss/**/*.scss'],
             tasks: ['sass:site', 'concat:cssSite', 'cssmin:site'],
             options: {
-                debounceDelay: 500,
+                debounceDelay: 500
             }
         }
     };
