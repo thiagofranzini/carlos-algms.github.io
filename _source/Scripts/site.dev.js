b0f297a38477a4e07590eef722d6bbdb666ad202
Carlos A. Gomes <carlos.algms@gmail.com>
2014-11-18 23:12:53 -0200

    github : basic format added method to update CSS without refresh the page

--- _source/Scripts/site.dev.js
+++ _source/Scripts/site.dev.js
@@ -238,10 +238,27 @@ dateToStr.REGEX = new RegExp('[' + Object.keys(dateToStr.MATCHES).join('') + ']'
 
 } )();;
 
-$(document).ready(function() {
+$(function() {
 	var each = function(i, block) {
 		hljs.highlightBlock(block);
 	};
 
 	$('pre code').each(each);
 });
+
+(function () {
+    var style = $('#devCss');
+
+    window.updateCss = function () {
+        style.attr('href', style.attr('href').replace(/\?*[0-9]*$/, '?' + Date.now()));
+        console.log('update-CSS');
+    };
+
+    $DOC.keyup(function (e) {
+        if (e.keyCode == 193) updateCss();
+    });
+
+    if (location.search.indexOf('refresh') !== -1) {
+        setInterval(window.updateCss, 1000);
+    }
+})();
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,25 @@
 
-$(document).ready(function() {
+$(function() {
 	var each = function(i, block) {
 		hljs.highlightBlock(block);
 	};
 
 	$('pre code').each(each);
 });
+
+(function () {
+    var style = $('#devCss');
+
+    window.updateCss = function () {
+        style.attr('href', style.attr('href').replace(/\?*[0-9]*$/, '?' + Date.now()));
+        console.log('update-CSS');
+    };
+
+    $DOC.keyup(function (e) {
+        if (e.keyCode == 193) updateCss();
+    });
+
+    if (location.search.indexOf('refresh') !== -1) {
+        setInterval(window.updateCss, 1000);
+    }
+})();
