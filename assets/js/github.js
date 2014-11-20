e3ea61330800ce3f5a2efec833338c725cf8b265
Carlos A. Gomes <carlos.algms@gmail.com>
2014-11-20 14:41:47 -0200

    changed good pratices and used spaces rather than TABs

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,86 +1,89 @@
 ( function (){
-	var Github = function (){
+  var Github = function (){
 
-		function escapeHtml( str ){
-			return $( '<div />' ).text( str ).html();
-		}
+    function escapeHtml( str ){
+      return $( '<div />' ).text( str ).html();
+    }
 
 
-		function _makeRepoLine( repo ) {
-		    var li = $('<li />'), a = $('<a />'), p = $('<p />');
+    function _makeRepoLine( repo ){
+      var li = $( '<li />' ), a = $( '<a />' ), p = $( '<p />' );
 
-		    li.append(a);
-		    li.append(p);
+      li.append( a );
+      li.append( p );
 
-		    a.attr('href', repo.html_url).html(repo.name);
-		    p.html( escapeHtml(repo.description || '') );
+      a.attr( 'href', repo.html_url ).html( repo.name );
+      p.html( escapeHtml( repo.description || '' ) );
 
-		    return li;
-		}
+      return li;
+    }
 
-		function render( targetId, repos ) {
-		    var target = $(targetId);
-		    target.empty();
+    function render( targetId, repos ){
+      var target = $( targetId );
+      target.empty();
 
-		    _.forEach(repos, function (repo) {
-		        target.append( _makeRepoLine(repo) );
-		    });
-		}
+      _.forEach( repos, function ( repo ){
+        target.append( _makeRepoLine( repo ) );
+      } );
+    }
 
-		function clearRepos( repos, options ) {
-		    var clean = _removeForks(repos, options);
-		    clean = _removeExceededRepos(clean, options);
-		    return clean;
-		}
+    function clearRepos( repos, options ){
+      var clean = _removeForks( repos, options );
+      clean = _removeExceededRepos( clean, options );
+      return clean;
+    }
 
-		function _removeForks( repos, options ) {
-			if( ! options.skip_forks ) return repos;
+    function _removeForks( repos, options ){
+      if( ! options.skip_forks )
+        return repos;
 
-			return _.filter(repos, function (repo) {
-			    return ! repo.fork;
-			});
-		}
+      return _.filter( repos, function ( repo ){
+        return ! repo.fork;
+      } );
+    }
 
-		function _removeExceededRepos(repos, options) {
-		    if (!options.count) return repos;
-		    return repos.splice(options.count);
-		}
+    function _removeExceededRepos( repos, options ){
+      if( ! options.count )
+        return repos;
+      return repos.splice( options.count );
+    }
 
-		return {
-		    showRepos : function ( options ){
-				$.ajax({
-					url : "https://api.github.com/users/" + options.user + "/repos?sort=pushed",
-					dataType: 'json',
-                    cache: true
-				})
-				.done(function (data) {
+    return {
+      showRepos : function ( options ){
+        $.ajax( {
+          url : "https://api.github.com/users/" + options.user + "/repos?sort=pushed",
+          dataType : 'json',
+          cache : true
+        } )
+            .done( function ( data ){
 
-					if( ! data ) return;
+              if( ! data )
+                return;
 
-					var repos = clearRepos(data, options);
+              var repos = clearRepos( data, options );
 
-					render( options.target, repos );
-				})
-				.fail(function ( err ) {
-					$( options.target + ' li.loading' ).addClass( 'error' ).text( "Error loading feed" );
-				});
-			}
-		};
-	};
+              render( options.target, repos );
+            } )
+            .fail( function ( err ){
+              $( options.target + ' li.loading' ).addClass( 'error' ).text( "Error loading feed" );
+            } );
+      }
+    };
+  };
 
-	$(function() {
+  $( function (){
 
-	    setTimeout(function() {
-		    var githubReposContainers = $('.github-repos-container');
+    setTimeout( function (){
+      var githubReposContainers = $( '.github-repos-container' );
 
-		    if( githubReposContainers.length ) {
-			    var github = Github();
+      if( githubReposContainers.length ){
+        var github = Github();
 
-			    $('.github-repos-container').each(function () {
-				    github.showRepos( $(this).data('data') );
-			    });
-		    }
-	    }, 1000);
-    });
+        $( '.github-repos-container' ).each( function (){
+          github.showRepos( $( this ).data( 'data' ) );
+        } );
+      }
+    }, 1000 );
+  } );
 
 } )();
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,25 +1,26 @@
 
-$(function() {
-	var each = function(i, block) {
-		hljs.highlightBlock(block);
-	};
+$( function (){
+  var each = function ( i, block ){
+    hljs.highlightBlock( block );
+  };
 
-	$('pre code').each(each);
-});
+  $( 'pre code' ).each( each );
+} );
 
-(function () {
-    var style = $('#devCss');
+( function (){
+  var style = $( '#devCss' );
 
-    window.updateCss = function () {
-        style.attr('href', style.attr('href').replace(/\?*[0-9]*$/, '?' + Date.now()));
-        console.log('update-CSS');
-    };
+  window.updateCss = function (){
+    style.attr( 'href', style.attr( 'href' ).replace( /\?*[0-9]*$/, '?' + Date.now() ) );
+    console.log( 'update-CSS' );
+  };
 
-    $DOC.keyup(function (e) {
-        if (e.keyCode == 193) updateCss();
-    });
+  $DOC.keyup( function ( e ){
+    if( e.keyCode == 193 )
+      updateCss();
+  } );
 
-    if (location.search.indexOf('refresh') !== -1) {
-        setInterval(window.updateCss, 1000);
-    }
-})();
+  if( location.search.indexOf( 'refresh' ) !== - 1 ){
+    setInterval( window.updateCss, 1000 );
+  }
+} )();
