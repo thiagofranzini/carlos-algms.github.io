eca7083c40e4587a39d10a19c6842f30a06a3bd5
Carlos A. Gomes <carlos.algms@gmail.com>
2015-07-19 13:06:59 -0300

    Fixed bootstrap js not included

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ function gruntConfig(grunt) {
           'assets/third-party/modernizr.min.js',
           'bower_components/lodash/dist/lodash.compat.min.js',
           'bower_components/jquery/dist/jquery.min.js',
-          'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js'
+          'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
         ],
         dest: '_source/Scripts/libs.dev.js'
       },
