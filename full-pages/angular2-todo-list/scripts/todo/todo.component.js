d3c793de648cd20f55c4d0c81de56cf5e97855f2
Carlos A. Gomes <carlos.algms@gmail.com>
2016-02-10 20:26:42 -0200

    New version of angular2-todo-list

---
+++ full-pages/angular2-todo-list/scripts/todo/todo.component.js
@@ -0,0 +1,45 @@
+System.register(['angular2/core', './todo.service', './todo-form.component'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, todo_service_1, todo_form_component_1;
+    var TodoComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (todo_service_1_1) {
+                todo_service_1 = todo_service_1_1;
+            },
+            function (todo_form_component_1_1) {
+                todo_form_component_1 = todo_form_component_1_1;
+            }],
+        execute: function() {
+            TodoComponent = (function () {
+                function TodoComponent(todoService) {
+                    this.todoService = todoService;
+                }
+                TodoComponent = __decorate([
+                    core_1.Component({
+                        selector: 'todo-list',
+                        providers: [todo_service_1.TodoService]
+                    }),
+                    core_1.View({
+                        template: "\n    <todo-form></todo-form>\n    <ul class=\"collection\" *ngIf=\"todoService.todos && todoService.todos.length > 0\">\n      <li class=\"collection-item\" *ngFor=\"#item of todoService.todos\">\n        <div>\n          {{ item.text }}\n          <a href=\"#!\" class=\"secondary-content\">\n            <i class=\"material-icons\">send</i>\n          </a>\n        </div>\n      </li>\n    </ul>\n  ",
+                        directives: [todo_form_component_1.TodoFormComponent]
+                    }), 
+                    __metadata('design:paramtypes', [todo_service_1.TodoService])
+                ], TodoComponent);
+                return TodoComponent;
+            })();
+            exports_1("TodoComponent", TodoComponent);
+        }
+    }
+});
--- full-pages/angular2-todo-list/scripts/todo/todo.service.js
+++ full-pages/angular2-todo-list/scripts/todo/todo.service.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', '../utils/storer'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,27 +8,38 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var AppComponent;
+    var core_1, storer_1;
+    var TodoService;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (storer_1_1) {
+                storer_1 = storer_1_1;
             }],
         execute: function() {
-            AppComponent = (function () {
-                function AppComponent() {
+            TodoService = (function () {
+                function TodoService() {
+                    var _this = this;
+                    this.storage = new storer_1.Storer('todo-items');
+                    this.storage.read().subscribe(function (todos) {
+                        _this.todos = todos || [];
+                    });
                 }
-                AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
-                    }), 
+                TodoService.prototype.add = function (todo) {
+                    this.todos.push(todo);
+                    todo.id = this.todos.length;
+                    this.storage.write(this.todos);
+                    return todo;
+                };
+                TodoService = __decorate([
+                    core_1.Injectable(), 
                     __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
+                ], TodoService);
+                return TodoService;
             })();
-            exports_1("AppComponent", AppComponent);
+            exports_1("TodoService", TodoService);
         }
     }
 });
