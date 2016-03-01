1973ebf3f3915319e5ee1a7d0401955bd5227acc
Carlos A. Gomes <carlos.algms@gmail.com>
2016-02-29 23:13:59 -0300

    Site updated: 2016-02-29 23:13:57

--- js/loadCss.js
+++ js/loadCss.js
@@ -1,18 +1,1 @@
-
-
-define(function() {
-  var head = document.getElementsByTagName('head')[0];
-  return loadCss;
-
-  ///////////////////
-
-  function loadCss(url, callback) {
-    var link = document.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.id = 'loadCss-' + Date.now();
-    link.href = url;
-    link.onload = callback;
-    head.appendChild(link);
-  }
-});
+define([],function(){function loadCss(url,callback){var link=document.createElement("link");link.type="text/css",link.rel="stylesheet",link.id="loadCss-"+Date.now(),link.href=url,link.onload=callback,head.appendChild(link)}var head=document.getElementsByTagName("head")[0];return loadCss});
